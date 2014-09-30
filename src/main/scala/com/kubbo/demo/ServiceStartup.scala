8cc1dc48b33bd9161f3327fd80482ec80848895d
zhuwei <1253300707@qq.com>
2014-10-01 01:29:22 +0800

    add demo serviceStartup class

---
+++ src/main/scala/com/kubbo/demo/ServiceStartup.scala
@@ -0,0 +1,15 @@
+package com.kubbo.demo
+
+import com.kubbo.rpc.akka.ProviderContainer
+
+/**
+ * Created by zhu on 2014/10/1.
+ */
+
+object ServiceStartup{
+  def main(args: Array[String]) {
+    val container = ProviderContainer()
+    container start(classOf[EchoService], new EchoServiceImpl(), "test", "1.0.0")
+    println("service start")
+  }
+}
