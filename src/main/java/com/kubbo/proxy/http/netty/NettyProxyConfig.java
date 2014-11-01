faa8f225af05c10fd133f07d894ccef78574ede2
zhuwei <1253300707@qq.com>
2014-11-01 13:40:30 +0800

    increase thread of akka remote

--- src/main/java/com/kubbo/proxy/http/netty/NettyProxyConfig.java
+++ src/main/java/com/kubbo/proxy/http/netty/NettyProxyConfig.java
@@ -17,13 +17,14 @@ public class NettyProxyConfig {
     private static final Logger logger = LoggerFactory.getLogger(NettyProxyConfig.class);
 
     private int workerCount;
-
+    private int bossCount;
     public static NettyProxyConfig load(String resourceName) {
 
-        Config innnerConfig = ConfigFactory.parseResources("proxy.conf");
-        logger.info("load config {}", innnerConfig.toString());
+        Config innerConfig = ConfigFactory.parseResources("proxy.conf");
+        logger.info("load config {}", innerConfig.toString());
         NettyProxyConfig nettyConfig = new NettyProxyConfig();
-        nettyConfig.setWorkerCount(innnerConfig.getInt("proxy.worker.count"));
+        nettyConfig.setWorkerCount(innerConfig.getInt("proxy.worker"));
+        nettyConfig.setBossCount(innerConfig.getInt("proxy.boss"));
         return nettyConfig;
     }
     public int getWorkerCount() {
@@ -32,5 +33,14 @@ public class NettyProxyConfig {
 
     public void setWorkerCount(int workerCount) {
         this.workerCount = workerCount;
+    }
+
+
+    public int getBossCount() {
+        return bossCount;
+    }
+
+    public void setBossCount(int bossCount) {
+        this.bossCount = bossCount;
     }
 }
